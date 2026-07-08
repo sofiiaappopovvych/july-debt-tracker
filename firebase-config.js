61f1bbcbdc121dd419cc59ac3f2ef51b4fec89d0
sofiiaappopovvych <skillfulsewing@gmail.com>
2026-07-08 09:59:19 -0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,8 @@
-// Paste your Firebase web app config here.
-// Example:
-// export const firebaseConfig = {
-//   apiKey: "...",
-//   authDomain: "...firebaseapp.com",
-//   projectId: "...",
-//   storageBucket: "...appspot.com",
-//   messagingSenderId: "...",
-//   appId: "..."
-// };
-
-export const firebaseConfig = null;
+const firebaseConfig = {
+  apiKey: "AIzaSyAA7sQnK7QLx08nw_97n9YB0Dd_bi8Ee_c",
+  authDomain: "july-debt.firebaseapp.com",
+  projectId: "july-debt",
+  storageBucket: "july-debt.firebasestorage.app",
+  messagingSenderId: "348071837476",
+  appId: "1:348071837476:web:5113a1665aae3c4a9dfdb1"
+};
